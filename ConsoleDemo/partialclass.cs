2677e61fc3912f896a1ac208dc6c70ee1da458da
SalimSiddiqui <Salimcyber786@gmail.com>
2018-07-31 21:00:55 +0530

    b

---
+++ ConsoleDemo/partialclass.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleDemo
+{
+   partial class partialclass
+    {
+        partial  void print();
+      
+        public void print1()
+        {
+
+        }
+    }
+    partial class partialclass
+    {
+       partial void print()
+        {
+
+        }
+    }
+    // showing error (can use partial method within same partial classs ,not in other class)
+    //partial class anothercalss
+    //{
+    //    partial void print()
+    //    {
+
+    //    }
+    //}
+}
