818fff2ce055a9472a6164a21fa620c23ed86f99
SalimSiddiqui <Salimcyber786@gmail.com>
2018-07-31 21:13:04 +0530

    a

---
+++ ConsoleDemo/Overloading_Overriding.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleDemo
+{
+    class Overloading_Overriding
+    {
+        public int add(int a, int b)
+        {
+            return 0;
+        }
+        //* return type will not be consider with same name and and same no of paramter in mehtod overloading//
+
+        //public float add(int a, int b)
+        //{
+        //    return 0;
+        //}
+    }
+}
