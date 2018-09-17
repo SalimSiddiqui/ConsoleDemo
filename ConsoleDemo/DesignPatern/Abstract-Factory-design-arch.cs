9fa1cd906733f9e1fa3174f9d3808fa8988e57c5
SalimSiddiqui <Salimcyber786@gmail.com>
2018-09-17 19:28:34 +0530

    b

---
+++ ConsoleDemo/DesignPatern/Abstract-Factory-design-arch.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleDemo.DesignPatern
+{
+    class Abstract_Factory_design_arch
+    {
+    }
+    public interface AbstractProductA { }
+    public class ProductA1 : AbstractProductA { }
+    public class ProductA2 : AbstractProductA { }
+    public interface AbstractProductB { }
+    public class ProductB1 : AbstractProductB { }
+    public class ProductB2 : AbstractProductB { }
+
+    public interface abstarctFactory
+    {
+        AbstractProductA CreateProductA();
+        AbstractProductB CreateProductB();
+
+    }
+
+    public class ConcreatefactoryA : abstarctFactory
+    {
+        public AbstractProductA CreateProductA()
+        {
+            return new ProductA1();
+        }
+
+        public AbstractProductB CreateProductB()
+        {
+            return new ProductB1();
+        }
+    }
+
+    public class ConcreatefactoryB : abstarctFactory
+    {
+        public AbstractProductA CreateProductA()
+        {
+            return new ProductA2();
+        }
+
+        public AbstractProductB CreateProductB()
+        {
+            return new ProductB2();
+        }
+
+        public class Client
+        {
+            private AbstractProductA _productA;
+            private AbstractProductB _productB;
+            public Client(abstarctFactory factory)
+            {
+                _productA = factory.CreateProductA();
+                _productB = factory.CreateProductB();
+
+
+            }
+
+        }
+    }
+}
---
+++ ConsoleDemo/DesignPatern/Abstract-Factory-design-imp.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleDemo.DesignPatern
+{
+    class Abstract_Factory_design_imp
+    {
+    }
+    public interface Vehcile
+    {
+        Bike getBike(string name);
+        Scooter getScooter(string name);
+    }
+
+    public interface Bike
+    {
+        void name();
+    }
+    public interface Scooter
+    {
+        void name();
+    }
+
+    public class HondaVehcle : Vehcile
+    {
+        public Bike getBike(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Scooter getScooter(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        
+    }
+}
