07bdfb56453c36bf1e24d1148081bc4507840103
SalimSiddiqui <Salimcyber786@gmail.com>
2018-10-29 18:57:23 +0530

    gf

--- ConsoleDemo/DesignPatern/FactoryMethodDesignPattern-arch.cs
+++ ConsoleDemo/DesignPatern/FactoryMethodDesignPattern-arch.cs
@@ -7,30 +7,60 @@ namespace ConsoleDemo.DesignPatern
 {
     class FactoryMethodDesignPattern_arch
     {
-    }
-  public  interface IProduct { }
-
-    public class ConcreateClassA : IProduct { }
-    public class ConcreateClassB : IProduct { }
-
-   public abstract class Creater
-    {
-        public abstract IProduct FactoryCreator(string Type);
-    }
-    public class ConcreateCreator : Creater
-    {
-        public override IProduct FactoryCreator(string Type)
+        public void Call()
         {
-            switch (Type)
-            {
-                case "A": return new ConcreateClassA();
-                case "B": return new ConcreateClassB();
 
-                default: throw new ArgumentException("Invalid Type");
-                    
+            // An array of creators
+
+            Creater[] creators = new Creater[2];
+
+            creators[0] = new ConcreateCreator();
+            creators[1] = new ConcreateCreator();
+
+            // Iterate over creators and create products
+
+            foreach (Creater creator in creators)
+            {
+                IProduct product = creator.FactoryCreator("A");
+                Console.WriteLine("Created {0}",
+                  product.GetType().Name);
+
+
+                 product = creator.FactoryCreator("B");
+                Console.WriteLine("Created {0}",
+                  product.GetType().Name);
             }
+
+            // Wait for user
+
+            Console.ReadKey();
+        }
+
+    }
+}
+public interface IProduct { }
+
+public class ConcreateClassA : IProduct { }
+public class ConcreateClassB : IProduct { }
+
+public abstract class Creater
+{
+    public abstract IProduct FactoryCreator(string Type);
+}
+public class ConcreateCreator : Creater
+{
+    public override IProduct FactoryCreator(string Type)
+    {
+        switch (Type)
+        {
+            case "A": return new ConcreateClassA();
+            case "B": return new ConcreateClassB();
+
+            default: throw new ArgumentException("Invalid Type");
+
         }
     }
+}
 
 
 }
