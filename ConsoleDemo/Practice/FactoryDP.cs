8a92005d08fd273d424edd2f7a39a5e7242dbaf9
Salim Siddiqui <Salimtcs@hotmail.com>
2023-06-18 13:55:54 +0530

    new2023

---
+++ ConsoleDemo/Practice/FactoryDP.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleDemo.Practice
+{
+    internal class FactoryDP
+    {
+    }
+
+
+    public interface IProduct { };
+    public class ProductAx : IProduct { };
+    public class ProductBx : IProduct { };
+
+    public abstract class Creatornew
+    {
+        public abstract IProduct ConcreateCreatorfactory(string type);
+    }
+
+
+    public class ConcreteCreator : Creatornew
+    {
+        public override IProduct ConcreateCreatorfactory(string type)
+        {
+
+            switch (type)
+            {
+                case "A": return new ProductAx();
+                case "B": return new ProductBx();
+                default: throw new ArgumentException("Invalid Type");
+
+            }
+        }
+    }
+}
+
+
+
+
